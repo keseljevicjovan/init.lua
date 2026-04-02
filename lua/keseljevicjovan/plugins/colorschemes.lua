b44fe00ba5eab8b7459ae87b9f54931f7a6de2cb
keseljevicjovan <keseljevicjovan@proton.me>
2026-04-02 03:03:11 +0200

    style(nvim): Clean up colorscheme config

--- lua/keseljevicjovan/plugins/colorschemes.lua
+++ lua/keseljevicjovan/plugins/colorschemes.lua
@@ -1,15 +1,7 @@
 return {
   'NTBBloodbath/doom-one.nvim',
   config = function()
-    vim.g.doom_one_cursor_coloring = false
-    vim.g.doom_one_terminal_colors = true
-    vim.g.doom_one_italic_comments = false
-    vim.g.doom_one_enable_treesitter = true
-    vim.g.doom_one_diagnostics_text_color = true
     vim.g.doom_one_transparent_background = true
-
-    vim.g.doom_one_pumblend_enable = true
-    vim.g.doom_one_pumblend_transparency = 20
 
     vim.cmd.colorscheme('doom-one')
   end
